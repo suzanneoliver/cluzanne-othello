a5a3314476188d0d762efedc5a552a1e6ae20bd1
suzanneoliver <suzanneoliverreally@hotmail.com>
2017-03-05 15:48:42 -0800

    makin a change

--- player.cpp
+++ player.cpp
@@ -1,5 +1,7 @@
 #include "player.hpp"
 
+// commiting some changes
+
 /*
  * Constructor for the player; initialize everything here. The side your AI is
  * on (BLACK or WHITE) is passed in as "side". The constructor must finish
